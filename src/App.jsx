e7a06ddea93fd2b67a168cd4dc28c945d7a1e08f
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-08-15 12:18:22 -0300

    Revert "atividade completa"
    This reverts commit 73d64afbe7663e90605898792f802dfc600acac8.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useContext } from "react"
-import { CreateTodoForm } from "./components/CreateTodoForm"
-import { TodoList } from "./components/TodoList"
-import { TodoContext } from "./providers/TodoContext"
-import { EditTodoForm } from "./components/EditTodoForm";
-
 function App() {
-  const { editingTodo } = useContext(TodoContext);
   return (
     <>
-      <CreateTodoForm />
-      <TodoList />
-      {editingTodo ? <EditTodoForm /> : null}
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { TodoProvider } from "./providers/TodoContext.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
-      <TodoProvider>
-         <App />
-      </TodoProvider>
-   </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
