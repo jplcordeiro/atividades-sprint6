4c42ea65c4c40ac73a7b25d8ada4798332455d0c
jplcordeiro <ctt.jplcordeiro@gmail.com>
2024-01-25 17:17:32 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import { CreateTodoForm } from "./components/CreateTodoForm"
 import { TodoList } from "./components/TodoList"
 import { TodoContext } from "./providers/TodoContext"
 import { EditTodoForm } from "./components/EditTodoForm";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+
+  const queryClient = new QueryClient();
+
   const { editingTodo } = useContext(TodoContext);
   return (
     <>
